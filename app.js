3b64863082eb11847ac043e81c0f6b9d29a14ef5
Michel Bigras <mgigrasnui@gmail.com>
2022-12-16 22:41:44 -0500

    lv1 lv2 .env

---
+++ app.js
@@ -0,0 +1,83 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+//console.log(process.env.API_KEY);
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
+res.render("home");
+});
+
+app.route("/login")
+
+.get(function(req, res) {
+res.render("login");
+})
+
+.post(function(req, res){
+
+  username = req.body.username; ;
+  password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        } else {res.render("login")}
+      } else {res.render("login")}
+    }
+  });
+})
+
+app.route("/register")
+
+.get(function(req, res) {
+  res.render("register");
+})
+
+.post(function(req, res){
+
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err){
+    if (!err){
+      res.render("secrets");
+    } else {
+      console.log(err);
+    }
+  });
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
