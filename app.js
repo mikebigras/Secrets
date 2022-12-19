8a24fdf0505c6ab73f502b2422914e6fa4c5d778
Michel Bigras <mgigrasnui@gmail.com>
2022-12-18 19:58:55 -0500

    add level 4

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -39,16 +40,16 @@ res.render("login");
 .post(function(req, res){
 
   username = req.body.username; ;
-  password = md5(req.body.password);
+  password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser){
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        } else {res.render("login")}
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {res.render("secrets");}
+        });
       } else {res.render("login")}
     }
   });
@@ -62,19 +63,22 @@ app.route("/register")
 
 .post(function(req, res){
 
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err){
+      if (!err){
+        res.render("secrets");
+      } else {
+        console.log(err);
+      }
+    });
   });
 
-  newUser.save(function(err){
-    if (!err){
-      res.render("secrets");
-    } else {
-      console.log(err);
-    }
-  });
-})
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
